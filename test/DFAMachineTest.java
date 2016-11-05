2f83c3371066444ce5f32be7864499cf1ee48afa
sitaram <sitaram@thoughtworks.com>
2016-11-05 12:37:07 +0530

    Added the test for those alphabet's which dfa machined will not be able to recognise.

--- test/DFAMachineTest.java
+++ test/DFAMachineTest.java
@@ -91,4 +91,60 @@ public class DFAMachineTest{
 
     }
 
+    @Test
+    public void DFAMachine_should_not_recognise__00() throws IllegalCharException {
+        State state1 = new State("q1");
+        State state2 = new State("q2");
+        state2.setFinalState(true);
+
+        Set<State> setOfStates = new HashSet<>();
+        setOfStates.add(state1);
+        setOfStates.add(state2);
+
+        Set<State> setOfFinalState = new HashSet<>();
+        setOfFinalState.add(state2);
+
+        HashSet alphabetSet = new HashSet<>();
+        alphabetSet.add('1');
+        alphabetSet.add('0');
+
+        TransitionFunction transitionFunction = new TransitionFunction();
+        transitionFunction.addTransition(new Transition(state1, '0', state2));
+        transitionFunction.addTransition(new Transition(state1, '1', state1));
+        transitionFunction.addTransition(new Transition(state2, '0', state1));
+        transitionFunction.addTransition(new Transition(state2, '1', state2));
+
+        DFAMachine machine = new DFAMachine(setOfStates,alphabetSet,transitionFunction,state1,setOfFinalState);
+        assertFalse(machine.check("00"));
+
+    }
+
+    @Test
+    public void DFAMachine_should_not_recognise__0000() throws IllegalCharException {
+        State state1 = new State("q1");
+        State state2 = new State("q2");
+        state2.setFinalState(true);
+
+        Set<State> setOfStates = new HashSet<>();
+        setOfStates.add(state1);
+        setOfStates.add(state2);
+
+        Set<State> setOfFinalState = new HashSet<>();
+        setOfFinalState.add(state2);
+
+        HashSet alphabetSet = new HashSet<>();
+        alphabetSet.add('1');
+        alphabetSet.add('0');
+
+        TransitionFunction transitionFunction = new TransitionFunction();
+        transitionFunction.addTransition(new Transition(state1, '0', state2));
+        transitionFunction.addTransition(new Transition(state1, '1', state1));
+        transitionFunction.addTransition(new Transition(state2, '0', state1));
+        transitionFunction.addTransition(new Transition(state2, '1', state2));
+
+        DFAMachine machine = new DFAMachine(setOfStates,alphabetSet,transitionFunction,state1,setOfFinalState);
+        assertFalse(machine.check("0000"));
+
+    }
+
 }
